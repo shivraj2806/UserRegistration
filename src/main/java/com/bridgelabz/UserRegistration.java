0fc8a0ca425da88e59198407385a32323e2ccd60
shivraj2806 <shivjadhav2806@gmail.com>
2021-09-04 21:43:43 +0530

    welcome message

---
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    }
+}
---
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRegistrationTest {
+    public static void main(String[] args) {
+        System.out.println("welcome to regular expression program");
+
+    }
+}
